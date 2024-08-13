ed951992532e9672dbe94d36e07d9f4cd6843283
JoaberTeixeira <joaberdasilva81@gmail.com>
2024-08-13 14:55:10 -0300

    add mensagens nas rotas

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ app.get('/Joaber', function(req,res){
   res.send('Oi Joáber!')
 })
 
+app.get('/:x', function(req,res){
+  const mensagem = 'Oi '+req.params.x
+  res.send(mensagem)
+})
+
+app.get('/:nome/:sobrenome', function(req,res){
+  const mensagem = 'Oi '+req.params.nome + ' ' + req.params.sobrenome
+  res.send(mensagem)
+})
+
 app.listen(3000)
