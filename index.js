97b5b211a9fbd1123eaf1e89cfadd8e4b1a0e620
JoaberTeixeira <joaberdasilva81@gmail.com>
2024-08-26 17:24:27 -0300

    add

--- index.js
+++ index.js
@@ -1,46 +1,61 @@
-import express from 'express'
-const app = express()
-
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended:true }))
-
-app.get('/', function(req,res){
-  //res.send('Oi mundo!')
-  res.render('tela1')
+import express from 'express';
+const app = express();
+app.set('view engine', 'ejs');
+//configura a pasta views para outro local
+//app.set('views', path.join(__dirname, 'tela'));
+app.use(express.urlencoded({ extended:true }));
+app.get('/',function (req,res){
+    //res.send("Oi mundo!");
+    res.render('tela1')
 })
 
-app.get('/Joaber', function(req,res){
-  res.send('Joáber!')
+app.post('/',function (req,res){
+
+    let resultado;
+    const val1 = parseFloat(req.body.val1);
+    const val2 = parseFloat(req.body.val2);
+    //const op = req.body.operacao;
+    if(req.body.operacao =="soma"){
+        resultado = val1+val2;
+    }else if(req.body.operacao=='subt'){
+        resultado = val1-val2;
+    }else if(req.body.operacao=='mult'){
+        resultado = val1*val2;
+    }else if(req.body.operacao=='divi'){
+        resultado = val1/val2;
+    }else{
+        res.send("Operação inválida")
+    }
+    res.send("O resultado da operação solicitada é: "+ resultado)
 })
 
-app.get('/:x', function(req,res){
-  const mensagem = 'Oi '+req.params.x
-  res.send(mensagem)
+app.get('/Diego',function(req,res){
+    res.send("Diego!");
 })
-
-app.get('/:nome/:sobrenome', function(req,res){
-  const mensagem = 'Oi '+req.params.nome + ' ' + req.params.sobrenome
-  res.send(mensagem)
+app.get('/:x',function(req,res){
+    const mensagem = "Oi "+req.params.x;
+    res.send(mensagem)
 })
-
-app.get('/:calc/:x/:y', function(req,res){
-  let calc
-  if(req.params.calc=="soma"){
-    calc = parseFloat(req.params.x) + parseFloat(req.params.y)
-  }
-  if(req.params.calc=="subt"){
-    calc = parseFloat(req.params.x) - parseFloat(req.params.y)
-  }
-  if(req.params.calc=="mult"){
-    calc = parseFloat(req.params.x) * parseFloat(req.params.y)
-  }
-  if(req.params.calc=="divi"){
-    calc = parseFloat(req.params.x) / parseFloat(req.params.y)
-  }
-  const mensagem = parseFloat(req.params.calc)
-  res.send(mensagem);
-  
+app.get('/:nome/:sobrenome',function(req,res){
+    const mensagem = "Oi "+req.params.nome+" "+req.params.sobrenome;
+    res.send(mensagem)
 })
-
-
-app.listen(3000)
+// Essa rota recebe dois valores e a operação e da a resposta em tela.
+app.get('/:calc/:n1/:n2',function(req,res){
+    let calc;
+    if(req.params.calc =="soma"){
+        calc = parseFloat(req.params.n1)+parseFloat(req.params.n2);
+    }else if(req.params.calc =="mult"){
+        calc = parseFloat(req.params.n1)*parseFloat(req.params.n2);
+    }else if(req.params.calc =="div"){
+        calc = parseFloat(req.params.n1)/parseFloat(req.params.n2);
+    }else if(req.params.calc =="subt"){
+        calc = parseFloat(req.params.n1)-parseFloat(req.params.n2);
+    }else{
+        res.send("Essa operação não pode ser executada")
+    }
+    //res.send("O resultado é "+calc)
+    res.send('resposta.ejs',{valor:resultado});
+                            
+})
+app.listen(3000);
